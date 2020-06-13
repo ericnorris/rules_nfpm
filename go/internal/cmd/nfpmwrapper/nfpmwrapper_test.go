d7bbd0925d7234caa645eb57ad400d0a4ecae4ee
Eric Norris <erictnorris@gmail.com>
2020-06-12 21:30:34 -0400

    Add tests for helper functions

--- go/internal/cmd/nfpmwrapper/nfpmwrapper_test.go
+++ go/internal/cmd/nfpmwrapper/nfpmwrapper_test.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 var (
@@ -16,6 +18,48 @@ var (
 const (
 	generateConfigDir = "./testdata/generateconfig"
 )
+
+var sortStringMapsOption = cmpopts.SortMaps(func(a, b string) bool { return a < b })
+
+func TestParseWorkspaceStatus(t *testing.T) {
+	workspaceStatusContents := `BUILD_EMBED_LABEL 
+BUILD_HOST hostname.example.com
+BUILD_USER user`
+
+	want := map[string]string{
+		"BUILD_EMBED_LABEL": "",
+		"BUILD_HOST":        "hostname.example.com",
+		"BUILD_USER":        "user",
+	}
+
+	got, err := parseWorkspaceStatus(ioutil.NopCloser(strings.NewReader(workspaceStatusContents)))
+
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if diff := cmp.Diff(want, got, sortStringMapsOption); diff != "" {
+		t.Errorf("parseWorkspaceStatus() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestParseDeps(t *testing.T) {
+	deps := []string{"//package:target=path/to/output"}
+
+	want := map[string]string{
+		"//package:target": "path/to/output",
+	}
+
+	got, err := parseDeps(deps)
+
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if diff := cmp.Diff(want, got, sortStringMapsOption); diff != "" {
+		t.Errorf("parseDeps() mismatch (-want +got):\n%s", diff)
+	}
+}
 
 func TestGenerateNFPMConfig(t *testing.T) {
 	cwd, _ := os.Getwd()
